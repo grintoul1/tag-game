f4d097544c1c33f008d38592bfabf8d5ac5891a5
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-03-29 19:01:28 -0500

    simplified acquisition of follower event flag

--- include/event_object_movement.h
+++ include/event_object_movement.h
@@ -248,6 +248,7 @@ const struct ObjectEventTemplate *GetObjectEventTemplateByLocalIdAndMap(u8 local
 u8 TrySpawnObjectEventTemplate(const struct ObjectEventTemplate *objectEventTemplate, u8 mapNum, u8 mapGroup, s16 cameraX, s16 cameraY);
 bool8 GetFollowerInfo(u16 *species, u8 *form, u8 *shiny);
 const struct ObjectEventGraphicsInfo *SpeciesToGraphicsInfo(u16 species, u8 form);
+u16 GetObjectEventFlagIdByLocalIdAndMap(u8 localId, u8 mapNum, u8 mapGroup);
 
 void MovementType_None(struct Sprite *);
 void MovementType_LookAround(struct Sprite *);
