a67840aa95c4ee9507c8029fb937356d2a010a8c
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-04-01 13:27:18 -0500

    renamed EscalatorMoveFollowerFinish to match new convention

--- src/field_effect.c
+++ src/field_effect.c
@@ -1849,7 +1849,9 @@ static bool8 EscalatorWarpIn_Init(struct Task *task)
     ObjectEventSetHeldMovement(objectEvent, GetFaceDirectionMovementAction(DIR_EAST));
     PlayerGetDestCoords(&x, &y);
     behavior = MapGridGetMetatileBehaviorAt(x, y);
-    EscalatorMoveFollowerFinish();
+#if OW_ENABLE_NPC_FOLLOWERS
+    EscalatorMoveFollowerNPCFinish();
+#endif
     task->tState++;
     task->data[1] = 16;
 
