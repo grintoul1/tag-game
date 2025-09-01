1f7c876e6cad644e2f05c7bb5c2c80b2a77dc020
grintoul1 <liam.rintoul@outlook.com>
2025-09-01 11:54:53 +0100

    Part 2

--- lua_rules.mk
+++
@@ -1,7 +0,0 @@
-# party files are run through trainerproc, which is a tool that converts party data to an output file
-# matching the current trainer .h formatting
-
-AUTO_GEN_TARGETS += src/luaproc/lua_script.lua
-
-%.lua: %.h
-	$(CPP) $(CPPFLAGS) -traditional-cpp - < $< | $(LUAPROC) -o $@ -i $< -
