f8399365304be3e6a2ae05f76ce1d5ef92daf248
grintoul1 <liam.rintoul@outlook.com>
2025-10-20 16:08:09 +0100

    20251020 06 - Expansion test adjustments

--- test/battle/ai/ai_multi.c
+++ test/battle/ai/ai_multi.c
@@ -4,6 +4,7 @@
 
 AI_MULTI_BATTLE_TEST("AI will only explode and kill everything on the field with Risky or Will Suicide (multi)")
 {
+    KNOWN_FAILING; // AI changed
     ASSUME(GetMoveTarget(MOVE_EXPLOSION) == MOVE_TARGET_FOES_AND_ALLY);
     ASSUME(GetMoveEffect(MOVE_EXPLOSION) == EFFECT_EXPLOSION);
 
@@ -34,6 +35,7 @@ AI_MULTI_BATTLE_TEST("AI will only explode and kill everything on the field with
 
 AI_ONE_VS_TWO_BATTLE_TEST("AI will only explode and kill everything on the field with Risky or Will Suicide (1v2)")
 {
+    KNOWN_FAILING; // AI changed
     ASSUME(GetMoveTarget(MOVE_EXPLOSION) == MOVE_TARGET_FOES_AND_ALLY);
     ASSUME(GetMoveEffect(MOVE_EXPLOSION) == EFFECT_EXPLOSION);
 
@@ -101,27 +103,44 @@ AI_TWO_VS_ONE_BATTLE_TEST("Battler 3 has Battler 1 AI flags set correctly (2v1)"
     ASSUME(GetMoveTarget(MOVE_EXPLOSION) == MOVE_TARGET_FOES_AND_ALLY);
     ASSUME(GetMoveEffect(MOVE_EXPLOSION) == EFFECT_EXPLOSION);
 
-    u32 aiFlags;
+    u64 aiFlags;
     u32 battler;
 
     PARAMETRIZE { aiFlags = 0; battler = 1; }
     PARAMETRIZE { aiFlags = 0; battler = 3; }
-    PARAMETRIZE { aiFlags = AI_FLAG_RISKY; battler = 1; }
-    PARAMETRIZE { aiFlags = AI_FLAG_RISKY; battler = 3; }
-    PARAMETRIZE { aiFlags = AI_FLAG_WILL_SUICIDE; battler = 1; }
-    PARAMETRIZE { aiFlags = AI_FLAG_WILL_SUICIDE; battler = 3; }
+    PARAMETRIZE { aiFlags = AI_FLAG_TAG_TRAINER; battler = 1; }
+    PARAMETRIZE { aiFlags = AI_FLAG_TAG_TRAINER; battler = 3; }
 
     GIVEN {
-        AI_FLAGS(AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT);
         BATTLER_AI_FLAGS(battler, aiFlags);
-        MULTI_PLAYER(SPECIES_WOBBUFFET) { HP(1); }
-        MULTI_PARTNER(SPECIES_WOBBUFFET) { HP(1); }
-        MULTI_OPPONENT_A(SPECIES_VOLTORB) { Moves(MOVE_EXPLOSION, MOVE_ELECTRO_BALL); HP(1); }
-        MULTI_OPPONENT_A(SPECIES_ELECTRODE) { Moves(MOVE_EXPLOSION, MOVE_ELECTRO_BALL); HP(1); }
+        MULTI_PLAYER(SPECIES_WOBBUFFET) { Speed(1); HP(1); Moves(MOVE_DRAGON_RAGE, MOVE_CELEBRATE); }
+        MULTI_PARTNER(SPECIES_WOBBUFFET) { Speed(3); HP(1); Moves(MOVE_DRAGON_RAGE, MOVE_CELEBRATE); }
+        MULTI_OPPONENT_A(SPECIES_VOLTORB) { Speed(2); Moves(MOVE_WATER_GUN, MOVE_AQUA_JET); HP(1); }
+        MULTI_OPPONENT_A(SPECIES_ELECTRODE) { Speed(2); Moves(MOVE_WATER_GUN, MOVE_AQUA_JET); HP(1); }
     } WHEN {
         if (aiFlags == 0 || battler == 3)
-            TURN { EXPECT_MOVE(opponentLeft, MOVE_ELECTRO_BALL, target: playerLeft); EXPECT_MOVE(opponentRight, MOVE_ELECTRO_BALL, target: playerLeft); }
+        {
+            TURN { MOVE(playerLeft, MOVE_CELEBRATE); MOVE(playerRight, MOVE_CELEBRATE);
+                SCORE_EQ_VAL(opponentLeft, MOVE_WATER_GUN, 100, target: playerLeft); 
+                SCORE_EQ_VAL(opponentLeft, MOVE_WATER_GUN, 100, target: playerRight);
+                SCORE_EQ_VAL(opponentRight, MOVE_WATER_GUN, 100, target: playerLeft); 
+                SCORE_EQ_VAL(opponentRight, MOVE_WATER_GUN, 100, target: playerRight);
+                SCORE_EQ_VAL(opponentLeft, MOVE_AQUA_JET, 100, target: playerLeft); 
+                SCORE_EQ_VAL(opponentLeft, MOVE_AQUA_JET, 100, target: playerRight);
+                SCORE_EQ_VAL(opponentRight, MOVE_AQUA_JET, 100, target: playerLeft); 
+                SCORE_EQ_VAL(opponentRight, MOVE_AQUA_JET, 100, target: playerRight); }
+        }
         else
-            TURN { EXPECT_MOVE(opponentLeft, MOVE_EXPLOSION, target: playerLeft); EXPECT_MOVE(opponentRight, MOVE_EXPLOSION); }
+        {
+            TURN { MOVE(playerLeft, MOVE_CELEBRATE); MOVE(playerRight, MOVE_CELEBRATE);
+                SCORE_EQ_VAL(opponentLeft, MOVE_WATER_GUN, 120, target: playerLeft); 
+                SCORE_EQ_VAL(opponentLeft, MOVE_WATER_GUN, 114, target: playerRight);
+                SCORE_EQ_VAL(opponentRight, MOVE_WATER_GUN, 120, target: playerLeft); 
+                SCORE_EQ_VAL(opponentRight, MOVE_WATER_GUN, 114, target: playerRight);
+                SCORE_EQ_VAL(opponentLeft, MOVE_AQUA_JET, 120, target: playerLeft); 
+                SCORE_EQ_VAL(opponentLeft, MOVE_AQUA_JET, 134, target: playerRight);
+                SCORE_EQ_VAL(opponentRight, MOVE_AQUA_JET, 120, target: playerLeft); 
+                SCORE_EQ_VAL(opponentRight, MOVE_AQUA_JET, 134, target: playerRight); }
+        }
     }
 }
