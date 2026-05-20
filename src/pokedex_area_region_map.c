227cd9354531025c4357a1e41b326a2cd4ee3c64
grintoul1 <liam.rintoul@outlook.com>
2026-05-20 21:09:01 +0200

    20260520 01 - INCGFX A2

--- src/pokedex_area_region_map.c
+++ src/pokedex_area_region_map.c
@@ -10,7 +10,7 @@
 
 #define POKEDEX_AREA_MAP_BG 3
 
-static const u32 sPokedexAreaMapAffine_Gfx[] = INCBIN_U32("graphics/pokedex/region_map_affine.8bpp.smol");
+static const u32 sPokedexAreaMapAffine_Gfx[] = INCGFX_U32("graphics/pokedex/region_map_affine.png", ".8bpp.smol", "-num_tiles 233 -Wnum_tiles");
 static const u32 sPokedexAreaMapAffine_Tilemap[] = INCBIN_U32("graphics/pokedex/region_map_affine.bin.smolTM");
 
 void LoadPokedexAreaMapGfx(void)
