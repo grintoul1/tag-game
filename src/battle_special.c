1af14d9e82d342f94518c527a4cad889de2aeeec
grintoul1 <liam.rintoul@outlook.com>
2026-09-20 21:19:00 +0200

    20260920 03 - Erika back sprite (credits Ramen/Dystopian)

--- src/battle_special.c
+++ src/battle_special.c
@@ -61,6 +61,7 @@ void CB2_EndSpecialTrainerBattle(void)
                     gSaveBlock1Ptr->playerParty[i] = gParties[B_TRAINER_PLAYER][i];
             }
         }
+        gSpecialVar_Result = gBattleOutcome;
         break;
     }
 
